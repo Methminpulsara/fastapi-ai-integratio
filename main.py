89462d5a55e0f69bae8acf1dadd60f504c3a85ea
Methminpulsara <methminpulsara10@gmail.com>
2025-11-01 12:08:25 +0530

    update

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import os
 import requests
 import json
 from fastapi import FastAPI
-from models import ChatRequest
+from models import ChatRequest, ChatResponse
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 # pip install fastapi
 # pip install uvicorn
@@ -16,29 +19,43 @@ load_dotenv()
 OPENROUTER_KEY = os.getenv("OPENROUTER_KEY")
 OPENROUTER_URL = os.getenv("OPENROUTER_URL")
 app = FastAPI(title='Ai Wrapper for openrouter api')
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # හෝ ["http://localhost:5173"]
+    allow_credentials=True,
+    allow_methods=["*"],  # POST, OPTIONS, GET හැමදෙයක්ම allow කරනවා
+    allow_headers=["*"],
+)
 
 def call_openrouter_api(message: str):
-
     headers = {
-            "Authorization": f"Bearer <{OPENROUTER_KEY}>",
-            "Content-Type": "application/json",
-        }
+        "Authorization": f"Bearer {OPENROUTER_KEY}",
+        "Content-Type": "application/json",
+    }
     body = {
-            "model": "meta-llama/llama-3.3-70b-instruct:free",
-            "messages": [
-                {
-                    "role": "user",
-                    "content": message
-                }
-            ],
+        "model": "meta-llama/llama-3.3-70b-instruct:free",
+        "messages": [
+            {
+                "role": "system",
+                "content": "You are an Helpful AI Assistant ,"
+                           "Who gives travel recommendations,"
+                           "on eco-friendly places in sri Lanka ,"
+                           ""
 
-        }
+            },
+            {
+                "role": "user",
+                "content": message
+            }
+        ]
+    }
 
-    response = requests.post(url=OPENROUTER_URL, headers=headers, data=body)
-    return response.json()
+    response = requests.post(url=OPENROUTER_URL, headers=headers, json=body)
+    return response.json()['choices'][0]['message']['content']
+
+
 
 
 @app.post('/chat')
 def chat(request: ChatRequest):
-    pass
+    return call_openrouter_api(request.message)
